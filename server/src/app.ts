2224325392e174c46da2ac02f8316d7d0bff8c21
Neeraj <neerajsaraswat111@gmail.com>
2025-04-27 01:11:35 +0530

    added server

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+import db from "../dbconnect";
+import dbpool from "../dbconnect";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors({ origin: "http://localhost:5173/" }));
+app.use(require("./routers/routes"));
+
+app.listen(process.env.PORT, () => console.log("Server running"));
