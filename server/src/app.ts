c8867f829d83ce16e2a73d81358c1ba9d2af6267
Neeraj <neerajsaraswat111@gmail.com>
2025-04-29 18:24:25 +0530

    changed cors origin path

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors({ origin: "https://devblog-lovat.vercel.app" }));
+app.use(cors({ origin: "https://devblog-lovat.vercel.app/" }));
 app.use(require("./routers/routes"));
 
 app.listen(process.env.PORT, () => console.log("Server running"));
