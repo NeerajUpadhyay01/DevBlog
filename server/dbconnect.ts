d06124c2d7fb72f682af5f6f1ac7a3f5e7f40868
Neeraj <neerajsaraswat111@gmail.com>
2025-04-30 22:11:56 +0530

    code working finally

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -1,15 +1,14 @@
 import dotenv from "dotenv";
 import { Pool } from "pg";
 dotenv.config();
-const DatabaseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.DB_ProdUrl
-    : process.env.DB_DevUrl;
+const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_Database}`;
 
 console.log(DatabaseUrl);
 
 const pool = new Pool({
   connectionString: DatabaseUrl,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 export default pool;
